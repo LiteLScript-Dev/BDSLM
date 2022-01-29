f55931c224ba94b343d5e38fc18a9eb48ebb7eb0
pa733 <2291750588@qq.com>
2022-01-29 19:05:04 +0800

    Change webserver
    将网页服务器更换为nginx.

--- BDSLM.js
+++ BDSLM.js
@@ -1,5 +1,20 @@
+var nginxConf = "\
+events {\n\
+    multi_accept       on;\n\
+}\n\
+\n\
+http {\n\
+    server {\n\
+        listen      port;\n\
+        listen      [::]:port;\n\
+        server_name example.com;\n\
+        root        ../unmined-web/;\n\
+        index       unmined.index.html;\n\
+    }\n\
+}"
+
 function GetServerProperties() {
-    var serverProperties = {};
+    let serverProperties = {};
     propertiesFile = File.readFrom("./server.properties").replace("\r", "").split(/\n/); // 读入server.properties并且分割每一行
     propertiesFile.forEach((item, _index) => {
         if (item && item.replace(" ", "").indexOf("#") != 0) {
@@ -11,14 +26,17 @@ function GetServerProperties() {
 }
 
 function RendMap() {
-    var serverProperties = GetServerProperties();
+    let serverProperties = GetServerProperties();
     system.cmd("start .\\plugins\\BDSLM\\unmined\\unmined-cli.exe web render --world=\"./worlds/" + serverProperties["level-name"] + "\" --output=\"./plugins/BDSLM/unmined-web/\" --imageformat=webp -c", function GetRendMapResult(_exitcode, _output) { });
-    startPythonWebserver();
+    startNginxWebserver();
 }
 
-function startPythonWebserver() {
+function startNginxWebserver() {
     port = JSON.parse(File.readFrom("./plugins/BDSLM/config.json"))["port"];
-    system.cmd("start .\\plugins\\BDSLM\\webserver.exe " + port, function GetRendMapResult(_exitcode, _output) { });
+    File.writeTo("./plugins/BDSLM/nginx/conf/nginx.conf", nginxConf.replaceAll("port", port));
+    system.cmd(".\\plugins\\BDSLM\\nginx\\nginx.exe -s stop -p ./plugins/BDSLM/nginx/", function GetRendMapResult(_exitcode, _output) {
+        system.newProcess(".\\plugins\\BDSLM\\nginx\\nginx -p ./plugins/BDSLM/nginx/", function GetRendMapResult(_exitcode, _output) { });
+    });
 }
 
 function Init() {
